04e9b2b0754d3a1b41edeb9f6dfcb7dfc3eaf378
Dmitry <gomerty15@mail.ru>
2021-04-19 12:52:16 +0300

    Добавленно решение

---
+++ zadacha-1/zadacha-1/zadacha-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	float a, b, c;
+	std::cin >> a >> b >> c;
+	if (a < 0 || b < 0 || c < 0) {
+		std::cerr << "error";
+		return 1;
+	}
+	else {
+		float p, s;
+		p = (a + b + c) / 2;
+		s = sqrt(p * (p - a) * (p - b) * (p - c));
+
+		std::cout << s << std::endl;
+	}
+	return 0;
+}
